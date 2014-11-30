6006a7a0101a80b318776f5e3375919e997c5137
Ben White <whtouche@gmail.com>
2014-11-29 20:20:42 -0500

    did lots of things

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
-var myApp = angular.module("StreetSweep",[]);
+var myApp = angular.module("StreetSweep",['ngResource']);
+
+
+myApp.service("streetList", function($resource) {
+    var parkingRestrictions = $resource('streetSweeping.json', {}).query();
+
+    function searchStreet(input) {
+        var searchResults = [];
+        for (var i = 0; i < parkingRestrictions.length; i++) {
+            if (parkingRestrictions[i]['Street Name'] === input) {
+                searchResults.push(parkingRestrictions[i]);
+            }
+        }
+        return searchResults;
+    }
+    return {search: searchStreet};
+});
 
 myApp.controller("parkingController", function($scope, streetList) {
     $scope.streetSearch = function() {
         $scope.foundStreet = streetList.search($scope.myStreet);
 
     };
-    $scope.daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday"];
-});
+    $scope.daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-myApp.service("streetList", function($resource) {
-    var parkingRestrictions = $resource(streetSweeping.json, {});
-    ];
-
-    function searchStreet(input) {
-        for (var i = 0; i < streets.length; i++) {
-            if (streets[i].name === input) {
-                return streets[i];
-            }
+    $scope.daysOfMonth = function() {
+        var days = [];
+        for(var length = 1; length < 32; length++) {
+                days.push( {date: length, side: 'even'} );
+                days.push( {date: length, side: 'odd'} );
         }
-        return null;
-    }
-    return {search: searchStreet};
+        return days;
+    }();
 });
+
+//create table where columns are days and rows are weeks
