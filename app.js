3fb34738336f01855e343fd4b712254d345efa17
Ben White <whtouche@gmail.com>
2014-12-02 22:10:04 -0500

    annotate code to find bug

--- app.js
+++ app.js
@@ -21,23 +21,29 @@ myApp.controller("parkingController", function($scope, streetList) {
         $scope.foundStreet = streetList.search($scope.myStreet);
 
     };
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    $scope.daysOfWeek = days;
+    var days            = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    $scope.daysOfWeek   = days;
 
-    $scope.daysOfMonth = getDaysOfMonth();
+    $scope.daysOfMonth  = getDaysOfMonth();
 
     function getDaysOfMonth() {
-        var months = [31,28,31,30,31,30,31,31,30,31,30,31];
-        var today = new Date();
-        var month = [];
-        var week = [];
-        var dayOfWeek = today.getDate() % 7;
+        var months      = [31,28,31,30,31,30,31,31,30,31,30,31];
+        var today       = new Date();
+        var month       = [];
+        var week        = [];
+        var dayOfWeek   = today.getDate() % 7;
+        //2
 
         for (var i = 0; i < months[today.getMonth()]; i++) {
+            //i < 31
             week[dayOfWeek] = {
+                //week[2]
                 day: days[dayOfWeek],
+                //days[2] (Tuesday)
                 date: i + 1,
+                //1
                 isToday: today.getDate() === i + 1
+                //False
             }
             if (dayOfWeek == 6) {
                 month.push(week);
